5ce3fbc1887be4f06cbef9c46e84f018d4a002c1
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2013-01-19 11:37:17 -0300

    Rename the app to TranslatorNotebook

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-ShoppingList::Application.load_tasks
+Translatornotebook::Application.load_tasks
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-ShoppingList::Application.routes.draw do
+Translatornotebook::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
