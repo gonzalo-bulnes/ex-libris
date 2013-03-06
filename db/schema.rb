9cde57c5d24802ef0cf4980a5ea84a777505741d
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2013-03-05 23:21:38 -0300

    Add Record model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130305023555) do
+ActiveRecord::Schema.define(:version => 20130306021552) do
 
   create_table "pages", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "records", :force => true do |t|
+    t.string   "location"
+    t.string   "picture"
+    t.string   "state"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
---
+++ spec/factories/records.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+
+  factory :record do
+    location "7G"
+    picture "http://placehold.it/130x130"
+    state "initial"
+  end
+end
